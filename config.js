6e9ce26e4e9749135e7894490da27af1f24db53d
Sheringford <59331637+Sheringford@users.noreply.github.com>
2025-12-05 22:52:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,31 +2,31 @@
 const projectPages = [
     {
         id: "page1",              // Unique ID for the URL
-        title: "Page 1",          // Display title
+        title: "Seite 1",          // Display title
         image: "0014.jpg",        // Filename in images/ folder
         xml: "transcription_0014.xml" // Filename in xml/ folder
     },
     {
         id: "page2",
-        title: "Page 2",
+        title: "Seite 2",
         image: "0015.jpg",
         xml: "transcription_0015.xml"
     },
     {
         id: "page3",
-        title: "Page 3",
+        title: "Seite 3",
         image: "0016.jpg",
         xml: "transcription_0016.xml"
     },
     {
         id: "page4",
-        title: "Page 4",
+        title: "Seite 4",
         image: "0017.jpg",
         xml: "transcription_0017.xml"
     },
     {
         id: "page5",
-        title: "Page 5",
+        title: "Seite 5",
         image: "0018.jpg",
         xml: "transcription_0018.xml"
     }
