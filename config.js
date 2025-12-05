91bc72f2e93ed50c4f966b544a4803c7853e415b
Sheringford <59331637+Sheringford@users.noreply.github.com>
2025-12-05 22:22:36 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js
+const projectPages = [
+    {
+        id: "page1",              // Unique ID for the URL
+        title: "Page 1",          // Display title
+        image: "0014.jpg",        // Filename in images/ folder
+        xml: "transcription_0014.xml" // Filename in xml/ folder
+    },
+    {
+        id: "page2",
+        title: "Page 2",
+        image: "0015.jpg",
+        xml: "transcription_0015.xml"
+    }
+    // To add a new page, copy the block above, add a comma, and paste it here!
+];
